43ba34ae0510f16e0a170798d1c0e7dbeaa80d56
jiminijr <jiminijr000106@gmail.com>
2024-01-24 21:58:15 +0900

    Backup

--- src/components/Levels/Level1.js
+++ src/components/Levels/Level1.js
@@ -4,21 +4,30 @@ import Webcam from "react-webcam";
 import { Camera } from "@mediapipe/camera_utils";
 import { Hands, Results } from "@mediapipe/hands";
 import { drawCanvas } from "../../components/Utils/drawCanvas";
+import { useDeferredValue } from 'react';
 
 const words = ['happy', 'love', 'good'];
+let socket; // socket 객체를 전역 스코프에서 정의
 
 const Level1 = () => {
+
+//   let currentLetterIndex=0;
+//   let currentWordIndex=0;
+//   currentLetterIndex = currentLetterIndex+1;
+//   currentWordIndex = currentWordIndex +1;
     
   const [currentWordIndex, setCurrentWordIndex] = useState(0);
   const [currentLetterIndex, setCurrentLetterIndex] = useState(0);
   const [inputValue, setInputValue] = useState('');
   const [correctCount, setCorrectCount] = useState(0);
   const [totalCount, setTotalCount] = useState(0);
+  const [wordCount, setWordCount] = useState(0);
   const [accuracy, setAccuracy] = useState(0);
   const [showPopup, setShowPopup] = useState(false);
   const [incorrectCount, setIncorrectCount] = useState(0);
   const [allWordsDisplayed, setAllWordsDisplayed] = useState(false);
   const [showButton, setShowButton] = useState(false); // 새로운 버튼 표시 여부 상태 추가
+  const [showModal, setShowModal] = useState(false);
   const [showSmallScreen, setShowSmallScreen] = useState(false); // 작은 화면 표시 여부 상태 추가
   const [nextWord, setNextWord] = useState(''); // 다음 word를 담는 상태 추가
   const [serverAnswer, setServerAnswer] = useState('');
@@ -27,167 +36,203 @@ const Level1 = () => {
   const socketRef = useRef(null);
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
+  const handsRef = useRef(null); // hands 객체를 useRef로 참조
 
 
-  const onResults = (results) => {
-    // MediaPipe 결과를 캔버스에 그리는 로직
+  const onResults = useCallback((results) => {
+    console.log(results, "called")
+    if (!results.multiHandLandmarks) {
+      console.log('No hands detected.');
+      return;
+    }
+
     const canvasElement = canvasRef.current;
     const videoElement = videoRef.current;
-    
-    if (canvasElement && videoElement) {
-      const ctx = canvasElement.getContext('2d');
-      ctx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-      ctx.drawImage(videoElement, 0, 0, canvasElement.width, canvasElement.height);
-      drawCanvas(ctx, results);
+    if (!canvasElement || !videoElement) {
+      console.log('Canvas or Video element not found.');
+      return;
     }
-  };
 
-
-
-
-  useEffect(() => {
-    const hands = new Hands({
-        locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`
-      });
-    
-      hands.setOptions({
-        maxNumHands: 2,
-        modelComplexity: 1,
-        minDetectionConfidence: 0.5,
-        minTrackingConfidence: 0.5
-      });
-    
-      hands.onResults(onResults);
-    
-      const socket = io('http://172.10.7.41:80', { withCredentials: true, transports: ['websocket'] });
-
-    
-      const startCamera = async () => {
-        try {
-          const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-          videoRef.current.srcObject = stream;
-  
-          videoRef.current.onloadedmetadata = () => {
-            videoRef.current.play();
-  
-            const captureImage = () => {
-              if (videoRef.current) {
-                const canvas = document.createElement('canvas');
-                const context = canvas.getContext('2d');
-                canvas.width = videoRef.current.videoWidth;
-                canvas.height = videoRef.current.videoHeight;
-                context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
-                const imageData = canvas.toDataURL('image/jpeg', 0.8);
-                socket.emit('prediction', imageData);
-              }
-            };
-  
-            const intervalId = setInterval(captureImage, 500);
-  
-            return () => clearInterval(intervalId);
-          };
-        } catch (error) {
-          console.error('웹캠 액세스 오류:', error);
-        }
-      };
-  
-      startCamera();
-
-      socket.on('prediction_result', (data) => {
-          console.log(data);
-          // 여기서 data를 활용하여 결과를 표시하거나 상태를 업데이트할 수 있습니다.
-      });
-
-      return () => {
-          socket.disconnect();
-      };
+    console.log('Hand landmarks:', results.multiHandLandmarks);
+    const ctx = canvasElement.getContext('2d');
+    ctx.save();
+    ctx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+    ctx.drawImage(videoElement, 0, 0, canvasElement.width, canvasElement.height);
+    // 미디어 파이프 결과를 그리는 코드 추가
+    drawCanvas(ctx, results.multiHandLandmarks);
+    ctx.restore();
   }, []);
 
   useEffect(() => {
+    const hands = new Hands({
+      locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`
+    });
+  
+    hands.setOptions({
+      maxNumHands: 2,
+      modelComplexity: 1,
+      minDetectionConfidence: 0.5,
+      minTrackingConfidence: 0.5
+    });
+
+    handsRef.current = hands; 
+  
+    // 웹캠 스트림 및 미디어 파이프 설정
+    if (webcamRef.current) {
+      const videoElement = webcamRef.current.video;
+      if (videoElement) {
+        videoElement.onloadedmetadata = () => {
+          // 미디어 파이프로 웹캠 프레임을 전달합니다.
+          hands.send({ image: videoElement });
+        };
+      }
+    }
+  
+    return () => {
+      hands.close(); // Hands 객체 정리
+    };
+  }, []); // 이 useEffect는 한 번만 실행됨
+
+  useEffect(() => {
+    if (handsRef.current) {
+        handsRef.current.onResults(onResults);
+      }
+    }, [onResults]);
+  
+
+  useEffect(() => {
+
     // 웹소켓 클라이언트 설정
-    socketRef.current = io('http://172.10.7.41:80', { withCredentials: true, transports: ['websocket'] });
+    socketRef.current = io('http://172.10.5.163:80', { withCredentials: true, transports: ['websocket'] });
 
     // 서버로부터 정답 알파벳 수신
     socketRef.current.on('answer', (answer) => {
       setServerAnswer(answer);
     });
 
+    const startCamera = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        videoRef.current.srcObject = stream;
+
+        videoRef.current.onloadedmetadata = () => {
+          videoRef.current.play();
+
+          const captureImage = () => {
+            if (videoRef.current) {
+              const canvas = document.createElement('canvas');
+              const context = canvas.getContext('2d');
+              canvas.width = videoRef.current.videoWidth;
+              canvas.height = videoRef.current.videoHeight;
+              context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
+              const imageData = canvas.toDataURL('image/jpeg', 0.8);
+              socketRef.current.emit('prediction', imageData);
+            }
+          };
+
+          const intervalId = setInterval(captureImage, 1000);
+
+          return () => clearInterval(intervalId);
+        };
+      } catch (error) {
+        console.error('웹캠 액세스 오류:', error);
+      }
+    };
+
+    startCamera();
+
+    socketRef.current.on('prediction_result', (data) => {
+      console.log(data);
+      // 여기서 data를 활용하여 결과를 표시하거나 상태를 업데이트할 수 있습니다.
+      console.log("received!");
+    });
+
     return () => {
       socketRef.current.disconnect();
     };
-  }, []);
+  }, [onResults]);
 
 
+  
+
+  //키보드 입력받은 값 처리하는 곳
   const handleKeyPress = (event) => {
     if (event.key === 'Enter') {
-      // 사용자 입력과 서버 응답 비교
-      if (inputValue.toLowerCase() === serverAnswer.toLowerCase()) {
+      if (inputValue.toLowerCase() === currentLetter.toLowerCase()) {
+        setInputValue('');
         setCorrectCount((prevCount) => prevCount + 1);
-        if (correctCount + 1 >= 5) {
-          console.log('정답!');
+        setTotalCount((prevCount) => prevCount + 1);
+
+        if (currentLetterIndex === currentWord.length - 1) {
+          setWordCount((prevIndex) => prevIndex + 1);
+          if (currentWordIndex === words.length - 1) {
+            setShowPopup(true);
+            setAllWordsDisplayed(true);
+            return;
+          } else {
+            setCurrentWordIndex((prevIndex) => (prevIndex + 1) % words.length);
+            setCurrentLetterIndex(0);
+          }
+        } else {
+          setCurrentLetterIndex((prevIndex) => prevIndex + 1);
         }
       } else {
-        console.log('오답');
+        setTotalCount((prevCount) => prevCount + 1);
       }
-      setInputValue('');
-      setTotalCount((prevCount) => prevCount + 1);
     }
   };
 
+  
+
   useEffect(() => {
-    let timeoutId; // 타이머 식별자를 저장할 변수
-  
-    const showSmallScreenTimer = () => {
-      setShowSmallScreen(true);
-      setNextWord(words[(currentWordIndex + 1) % words.length]); // 다음 word 설정
-  
-      // 입력 받는 시간 설정
-      const inputTime = currentWordIndex === 0 ? 10000 : 5000;
-  
-      timeoutId = setTimeout(() => {
-        setShowSmallScreen(false);
-        setNextWord(''); // 다음 word 초기화
-  
-      }, inputTime);
-    };
+    let timeoutId;
 
     const mainTimer = () => {
       if (currentLetterIndex === words[currentWordIndex].length - 1) {
         setCurrentWordIndex((prevIndex) => (prevIndex + 1) % words.length);
         setCurrentLetterIndex(0);
         setInputValue('');
-        setIncorrectCount(0);
-  
-        if (currentWordIndex === words.length - 1 && allWordsDisplayed) {
-          // 수정: 정답률 계산을 현재까지의 맞춘 철자 수로 변경
-          // const newAccuracy = (correctCount / totalCount) * 100;
-          // setAccuracy(newAccuracy);
-          console.log("useeffect 최종 갯수 :", correctCount);
-          console.log("useeffect 총 count 갯수 :", totalCount);
+        setWordCount((prevIndex) => prevIndex + 1);
+
+        if (wordCount === words.length && currentWordIndex === words.length - 1 && allWordsDisplayed) {
+          const newAccuracy = (correctCount / totalCount) * 100;
+          setAccuracy(newAccuracy);
           setShowPopup(true);
-          // 새로운 버튼 표시
           setShowButton(true);
-          // 수정: 모든 단어를 표시했음을 초기화
           setAllWordsDisplayed(true);
+          const inputTime = currentLetterIndex === 0 ? 15000 : 10000;
 
           setTimeout(() => {
             setCurrentWordIndex((prevIndex) => (prevIndex + 1) % words.length);
-          }, 5000);
+          }, inputTime);
         }
       } else {
         setCurrentLetterIndex((prevIndex) => prevIndex + 1);
       }
       setTotalCount((prevCount) => prevCount + 1);
-      // 작은 화면 타이머 재시작
-      showSmallScreenTimer();
     };
 
-    timeoutId = setTimeout(mainTimer, 3000); // 초기 실행
+    timeoutId = setTimeout(mainTimer, currentLetterIndex === 0 ? 15000 : 10000);
 
     return () => {
       clearTimeout(timeoutId);
     };
   }, [currentWordIndex, currentLetterIndex, allWordsDisplayed, correctCount, totalCount, showPopup]);
+
+  // Modal 창 띄우는 곳
+  useEffect(() => {
+    if (currentLetterIndex === 0) {
+      setShowModal(true);
+
+      const modalTimeoutId = setTimeout(() => {
+        setShowModal(false);
+      }, 3000);
+
+      return () => {
+        clearTimeout(modalTimeoutId);
+      };
+    }
+  }, [currentLetterIndex]);
 
   const currentWord = words[currentWordIndex];
   const currentLetter = currentWord[currentLetterIndex];
@@ -196,6 +241,9 @@ const Level1 = () => {
     setInputValue(event.target.value);
   };
 
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
 
   return (
 <div style={{ display: 'flex', height: '100vh', width: '100vw' }}>
@@ -223,16 +271,18 @@ const Level1 = () => {
       <div>
         <p>정답률: {accuracy.toFixed(2)}%</p>
         {showButton && (
-          <div>
-            <button className='button_menu' onClick={() => console.log('New Button Clicked')}> 메뉴 </button>
-            <button>다음 레벨 </button>
-          </div>
+            <button className='button_menu' onClick={() => console.log('New Button Clicked')}> 처음으로 </button>
         )}
       </div>
     )}
   </div>
 
   <div style={{ flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        {!allWordsDisplayed && showModal && (
+          <div style={{ width: '700px', height: '500px', position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', padding: '20px', background: '#fff', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+            <p style={{ fontSize: '4em' }}>Next Word: {currentWord}</p>
+          </div>
+        )}
   {/* <div>
         <canvas
           ref={canvasRef}
@@ -240,12 +290,12 @@ const Level1 = () => {
           height={1000}
         />
       </div> */}
-    <div>
-      <div className="video-container">
-    <video ref={videoRef} width="50vw" height="100vh" autoPlay></video>
-  </div>
+ {/* 웹캠 및 캔버스 */}
+ <div className="video-container" style={{ position: 'relative' }}>
+        <video ref={videoRef} style={{ width: '100%', height: '100%' }} autoPlay></video>
+        <canvas ref={canvasRef} width={640} height={480} style={{ position: 'absolute', top: '0', left: '0' }} />
+      </div>
 
-    </div>
   </div>
 </div>
 
